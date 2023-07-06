b5340c40d8a24913548a8a1b2fe2c2667280cd23
Himanshusharama <honeysharma9964@gmail.com>
2023-07-06 16:54:25 +0530

    Video Tag finding

--- scrapper.py
+++ scrapper.py
@@ -1,15 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 
 
 YOUTUBE_TRENDING = 'https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl'
+def get_driver():
+ chrome_options=Options()
+ chrome_options.add_argument('--no-sandbox')
+ chrome_options.add_argument('--disable-dev-shm-usage')
+ chrome_options.add_argument('--headless')
+ driver = webdriver.Chrome( options=chrome_options)
+ return driver 
 
-chrome_options=Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--headless')
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-driver.get(YOUTUBE_TRENDING)
-print ('page title ', driver.title)
+
+if __name__=="__main__":
+ driver.get(YOUTUBE_TRENDING)
+ print ('page title ', driver.title)
