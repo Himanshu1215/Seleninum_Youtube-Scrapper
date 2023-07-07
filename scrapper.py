a1a0c6f2c4cb6e93e7fec2ed041c7ce60d894677
Himanshusharama <honeysharma9964@gmail.com>
2023-07-07 12:17:45 +0530

    Extracted top 20 videos

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-
+import pandas as pd 
 
 
 
@@ -23,8 +23,37 @@ def get_video(driver):
    elements = driver.find_elements(By.TAG_NAME, 'ytd-video-renderer')
    return elements
 
+def parse_video(video):
+    title_element = video.find_element(By.ID, 'video-title')
+    title_tag = title_element.text
+    link = title_element.get_attribute('href')
+    
+    thumbnail_tags = video.find_elements(By.TAG_NAME, 'img')
+
+    thumbnail_urls = [thumbnail.get_attribute('src') for thumbnail in thumbnail_tags]
+    
+    channel_div = video.find_element(By.CLASS_NAME , 'ytd-channel-name')
+    channel_name= channel_div.text
+
+    
+    channel_description= video.find_element(By.ID , 'description-text')
+    description = channel_description.text
+    
+    
+    video_views = video.find_elements(By.CSS_SELECTOR, 'span.inline-metadata-item')
+    view = video_views[0].text
+    time = video_views[1].text
+    return {
+       'title': title_tag,
+       'url'  : link,
+       'thumbnail_tag':thumbnail_urls,
+       'channel_name':channel_name,
+       'description ' : description,
+       'view' : view,
+       'time':time
 
 
+    }
 
 if __name__ == "__main__":
     print('creating driver')
@@ -38,9 +67,18 @@ if __name__ == "__main__":
 
     print(f'found {len(videos)} videos')
     
-    print('Parsing The first Video')
-    # titile ,Views ,Uploaded, description, chanel ,url 
+    print('Parsing The  Videos')
+    # titile ,Views ,Uploaded, description, chanel ,url
 
-    video = videos[0]
-    title= video.find_element(By.ID,'video-title').text
-    print(title)
+    videos_data = [parse_video(video) for video in videos[:20] ]
+    
+    
+
+    print('save data to csv')
+    videos_df = pd.DataFrame(videos_data)
+    print(videos_df)
+
+    videos_df.to_csv('trending.csv', index=None)
+
+
+    
