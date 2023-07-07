cfcafc3f20c61ba6e32526b2864e492b5c44f5fc
Himanshusharama <honeysharma9964@gmail.com>
2023-07-07 12:17:38 +0530

    Exctarcted top 20 videos

--- scrapper.py
+++ scrapper.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 
 
 
 YOUTUBE_TRENDING = 'https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl'
+
+
 def get_driver():
  chrome_options=Options()
  chrome_options.add_argument('--no-sandbox')
@@ -14,6 +17,30 @@ def get_driver():
  return driver 
 
 
-if __name__=="__main__":
- driver.get(YOUTUBE_TRENDING)
- print ('page title ', driver.title)
+
+def get_video(driver):
+   driver.get(YOUTUBE_TRENDING)
+   elements = driver.find_elements(By.TAG_NAME, 'ytd-video-renderer')
+   return elements
+
+
+
+
+if __name__ == "__main__":
+    print('creating driver')
+
+    driver = get_driver()
+    print('Fetching the page ')
+
+    
+    print('trending Video')
+    videos=get_video(driver)
+
+    print(f'found {len(videos)} videos')
+    
+    print('Parsing The first Video')
+    # titile ,Views ,Uploaded, description, chanel ,url 
+
+    video = videos[0]
+    title= video.find_element(By.ID,'video-title').text
+    print(title)
